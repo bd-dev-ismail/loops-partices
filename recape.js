2a3427c3dcb5663fd94e0fd6077ee64701fac004
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-01 22:23:00 +0600

    first commit

---
+++ recape.js
@@ -0,0 +1,7 @@
+var bottleFor = 'water';
+var bottleColor = 'yellow';
+var sunGlassPrice = '120';
+var penColor = 'blue';
+var isExpensive = true;
+
+var tableItems = ['pen', 'headphone', 'khata', 'water bottle'];
