2944db664b71682b05d4af3bbd659c40ff864233
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-01 22:48:55 +0600

    While loop

--- recape.js
+++ recape.js
@@ -5,3 +5,12 @@ var penColor = 'blue';
 var isExpensive = true;
 
 var tableItems = ['pen', 'headphone', 'khata', 'water bottle'];
+if(tableItems.length > 4){
+
+}
+else if(tableItems[3] === 'water bottle') {
+
+}
+else{
+
+}
---
+++ while.js
@@ -0,0 +1,11 @@
+var biriyani = 0;
+while(biriyani < 7){
+    // onsole.log('Biriyani den, Please');
+    // console.log(biriyani);c
+    biriyani++;
+}
+var num = 0;
+while(num < 5){
+    console.log('Hello');
+    num++;
+}
