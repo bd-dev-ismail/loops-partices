e7ecca63717988c6ff17c5148140850128bca7c3
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-02 06:44:43 +0600

    Run loop in array

---
+++ array-loop.js
@@ -0,0 +1,14 @@
+// for(var i = 0; i<10; i++){
+//     console.log(i);
+// }
+//Taget to display every element
+// var numbers = [43, 54, 76, 20, 34, 78, 95, 234, 36, 75];
+// for(var i =0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+var tableItems = ['bottle', 'mobile', 'headphone', 'mouse', 'medicine', 'khata'];
+for(i = 0; i < tableItems.length; i++){
+    var tableItem = tableItems[i];
+    console.log(tableItem);
+}
