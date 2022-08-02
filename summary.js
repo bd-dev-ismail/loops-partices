73b980101f9e2006ce37e6d09d408a49060b212a
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-02 21:37:24 +0600

    partics js

--- summary.js
+++ summary.js
@@ -1,11 +1,27 @@
-var num = 0;
-while(num < 10){
-    console.log(num);
-    num++;
+// var num = 0;
+// while(num < 10){
+//     console.log(num);
+//     num++;
+// }
+// for(i = 0; i <=10; i++){
+//     console.log(i);
+//     if(i <5){
+//         break;
+//     }
+// }
+var num = "72.50";
+// console.log(parseFloat(num));
+
+var hero = 'Badsha'
+if(hero == 'Hero Alam'){
+    console.log('Marbo bogurai lash porbe magurai');
 }
-for(i = 0; i <=10; i++){
-    console.log(i);
-    if(i <5){
-        break;
-    }
+else if(hero == 'Mitun Dash'){
+    console.log('Marbo ekane lash probe shosane');
+}
+else if(hero == 'Badsha khan'){
+    console.log('Marbo chittagong lash prbe kobore');
+}
+else {
+    console.log('Maramari Kora valo na');
 }
