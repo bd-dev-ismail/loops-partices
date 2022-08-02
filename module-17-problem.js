a51c45bd69070c5a5c3fbe0ead3ae453d33a225d
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-02 09:21:13 +0600

    solving problem

---
+++ module-17-problem.js
@@ -0,0 +1,8 @@
+var fruts = ['apple', 'banana', 'orange'];
+var bananaIndex = fruts.indexOf('banana');
+fruts[1] = 'mango';
+console.log(bananaIndex);
+fruts.pop();
+console.log(fruts);
+fruts.push('Watermelon');
+console.log(fruts);
